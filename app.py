233da0d5b5705241bbc094b974573c99bc612d5c
varad <varadkhonde@gmail.com>
2023-07-20 21:08:51 +0530

    app.py added

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route("/test", methods=['POST'])
+def test():
+    if request.method == 'POST':
+        return jsonify({'status' : 200, 'msg' : 'success'})
+
